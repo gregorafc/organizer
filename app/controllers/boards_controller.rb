b58c63bacc67dd17cc048d865dcac64fdeedfbdb
Grzegorz <gregor.afc@gmail.com>
2016-02-13 22:20:05 +0100

    Add CRUD for boards

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,51 @@
 class BoardsController < ApplicationController
-
+  before_filter :set_board, only: [:show, :edit, :update, :destroy]
 
   def index
     @boards = Board.all
+  end
+
+  def show
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to @board, notice: 'Board was succesfully creates.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @board.update(board_params)
+      redirect_to @board, notice: 'Board was succesfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @board.destroy
+    redirect_to boards_path, notice: 'Board was succesfully destroyed.'
+  end
+
+
+  private
+
+  def set_board
+    @board = Board.find(params[:id])
+  end
+
+  def board_params
+    params.require(:board).permit(:title)
   end
 
 end
